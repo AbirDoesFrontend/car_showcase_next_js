461db31131e1dd50ba6b67793cac9422f2ea80dc
Abir Khan <abirkhan.frontend.dev24@gmail.com>
2023-06-18 13:42:56 +0600

    Car Images: Imagin Studio API

--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,12 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
   const headers = {
       "X-RapidAPI-Key": "1df0a3a0b9msh2a61be70d58c356p16e2b0jsn8a37d1537fcb",
       "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com"
   };
 
-  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla' , {
+  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3' , {
     headers : headers,
   })
 
@@ -23,4 +25,19 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRatePerDay.toFixed(0);
-};
+}
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+  const { make, model, year } = car;
+
+  url.searchParams.append('customer', 'bdabir-does-frontend');
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(" ")[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
+} 
